1d8f474de6c11a7e11eaa669093769df46c53de8
Nadzhmeddin <xside55@yandex.ru>
2023-03-07 11:02:09 +0300

    Task 2 1_1

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+int num1 = a;
+int num2 = b;
+
+
+Console.Write("Please enter number1 n1 =");
+num1 = int.Parse(Console.ReadLine()!));
+
+Console.Write("Please enter number2 n2 =");
+num2 = int.Parse(Console.ReadLine()!));
+
+if ((num2*num2) == num1)
