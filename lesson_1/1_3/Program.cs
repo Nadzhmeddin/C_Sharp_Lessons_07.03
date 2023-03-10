ae758b1d2c4cd3c3214fc7e1a23f5607cd112942
Nadzhmeddin <xside55@yandex.ru>
2023-03-10 09:43:29 +0300

    Задача 1_3

---
+++ lesson_1/1_3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Ввведите число N =");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n < 0)
+{
+    n = -n;
+}
+
+int count = -n;
+while (count != n)
+{
+    Console.WriteLine(count);
+    if (n < 0)
+    {
+        count--;
+    }
+else
+{
+    count++;
+}
+}
+
+
