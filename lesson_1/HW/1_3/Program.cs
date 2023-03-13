5eaeadc1ef6d8888a117705cfab848b84d328847
Nadzhmeddin <xside55@yandex.ru>
2023-03-13 22:51:34 +0300

    HW 1_3 добавили в ветку 1_lesson

--- lesson_1/HW/1_3/Program.cs
+++ lesson_1/HW/1_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число четным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+
+if (numberA % 2 == 0 )
+{
+    Console.WriteLine("Четное число");
+}
+else 
+{
+    Console.WriteLine("Нечётное число");
+}
