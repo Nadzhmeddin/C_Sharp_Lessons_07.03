0b3105c6140d67b210443aff32b7e6d6243da892
Nadzhmeddin <xside55@yandex.ru>
2023-03-13 23:25:16 +0300

    Task 2_1

--- lesson_2/2_1/Program.cs
+++ lesson_2/2_1/Program.cs
@@ -1,13 +1,40 @@
 ﻿// Напишите программу, которая выводит случайное число из 
 // отрезка [10, 99] и показывает наибольшую цифру числа.
 
-int GetNum();
+int num = GenerateNumber();
+PrintNumber(num);
+int digit1 = GetFirstDigit(num);
+int digit2 = GetSecondDigit(num);
+int max = FindMax(digit1 , digit2);
+PrintNumber(max);
+
+int GenerateNumber()
 {
-    int number = new Random().New(10, 100);
+    int number = new Random().Next(10, 100);
     return number;
 }
-int Second (int n)
+int GetSecondDigit(int n)
 {
-    return n%10;
+    return n % 10;
 }
+
+int GetFirstDigit(int n)
+{
+    return n / 10;
+}
+
+int FindMax (int n1, int n2)
+{
+if (n1 > n2)
+{
+    return n1;
+}
+return n2;
+}
+
+void PrintNumber( int n)
+{
+    Console.WriteLine("Max number is: " + n);
+}
+
 
