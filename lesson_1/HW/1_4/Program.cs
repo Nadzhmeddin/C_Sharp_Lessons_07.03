7ebd5e71663b6599f9bdcdb8321bad2c671f3a2f
Nadzhmeddin <xside55@yandex.ru>
2023-03-13 20:34:23 +0300

    HW 1_4

---
+++ lesson_1/HW/1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N.
+
+
+int numberA = int.Parse(Console.ReadLine()!);
+int index = 2;
+
+while (index <= numberA)
+{
+    Console.Write($"{index}");
+    index += 2;
+}
