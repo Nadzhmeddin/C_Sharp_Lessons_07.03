dc51c2c6b7788b6a785663731c13da704fad8720
Nadzhmeddin <xside55@yandex.ru>
2023-03-24 09:48:56 +0300

    4_3

---
+++ lesson_4/HW/4_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// случайными числами и выдаёт их на экран. Оформите заполнение массива
+// и вывод в виде функции (пригодится в следующих задачах)
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for(int i = 0; i < arr.Length; i++)
+        Console.Write($" {arr[i]}");
+    Console.WriteLine();
+}
+
+int[] EightMass(int size, int from, int to)
+{
+    int[] arr  = new int [size];
+
+    for(int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = EightMass(num, start, stop);
+Print(mass);
