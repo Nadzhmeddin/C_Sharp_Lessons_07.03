378d764336d66122def0a1fb68fdde7a13604362
Nadzhmeddin <xside55@yandex.ru>
2023-03-28 09:14:46 +0300

    5_

--- lesson_5/5_2/Program.cs
+++ lesson_5/5_2/Program.cs
@@ -1,2 +1,44 @@
+// 
 
-Console.WriteLine(FindNumber(array, n));
+
+
+
+Console.Write("Кол-во элементов:");
+int count =int.Parse(Console.ReadLine()!);
+Console.Write("Начало диапозона:");
+int a =int.Parse(Console.ReadLine()!);
+Console.Write("Конец диапозона:");
+int b =int.Parse(Console.ReadLine()!);
+
+int[] MakeArray(int c, int m, int n){
+    int[] array =new int[c];
+
+    for (int i=0;i<array.Length;i++) array[i]=new Random().Next(m,n+1);
+    return array;
+}
+
+void PrintArray(int[] arr){
+    for (int i=0;i<arr.Length;i++) Console.Write($"{arr[i]} ");
+}
+
+int[] array = MakeArray(count, a, b);
+PrintArray(array);
+
+
+bool FindNumber(int[] arr, int n)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]==n)
+        {
+            return true;
+        }
+    
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(FindNumber(array,n));
