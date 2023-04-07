3de9c9b3ef661b8935d7f1cce7ca9b4638b28d6a
Nadzhmeddin <xside55@yandex.ru>
2023-04-07 09:02:49 +0300

    7_3

---
+++ lesson_7/7_3/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задайте двумерный массив. Найдите сумму элементов главной диагонали.   
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)      
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to + 1);                
+    
+    return arr;
+}
+
+int Sum(int[,] arr)
+{
+    int SumNum = 0;
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            if( i ==j) SumNum += arr[i,j];
+        }
+    }
+    return SumNum;
+}
+
+
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+Sum(mass);
