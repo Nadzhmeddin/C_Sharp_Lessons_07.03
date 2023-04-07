d4df13385b0625e89cff3d492888a38adffff769
Nadzhmeddin <xside55@yandex.ru>
2023-04-07 09:22:37 +0300

    HW 7_2

---
+++ lesson_7/HW/7_2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)        
+            Console.Write($" {arr[i, j]} ");        
+        Console.WriteLine();       
+    }
+    Console.WriteLine();
+}
+
+int [,] MassNums(int row, int column, int from, int to)
+{
+    int [,] arr = new int [row, column];
+
+    for (int i = 0; i < row; i++)      
+        for (int j = 0; j < column; j++)        
+            arr[i, j] = new Random().Next(from, to + 1);                
+    
+    return arr;
+}
+
+string FindNum (int [,] arr, int a, int b)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if(a > row || a <=0 || b > column || b <=0)
+        return $"{a} {b} - не найден в массиве";
+    return $"arr[{a},{b}] = {arr[a-1, b-1]} найден в массиве";
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+
+Print(mass);
