b476fc1f1bc60dc1e54f64cacf10d16cc29cbac1
Nadzhmeddin <xside55@yandex.ru>
2023-04-11 01:25:04 +0300

    9_0

--- lesson_9/9_1/Program.cs
+++ lesson_9/9_1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Задайте значения M и N. напишите программу, 
+//которая рекурсивно выдает все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите число M = ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine()!);
+
+void PrintNumbers (int a, int b)
+{
+    if(a -1 ==b)
+    {
+        return;
+    }
+    PrintNumbers(a,b-1);
+    Console.Write($"{b} ");
+}
+
+PrintNumbers(M,N);
